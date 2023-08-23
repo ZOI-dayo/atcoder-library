4934ac2ef705b5676a0566f357c92b96f4c5b5ee
ZOI_dayo <zoizoi.dayo@gmail.com>
2023-08-23 18:57:18 +0900

    UnionFindにgroups実装

--- datastructure/unionfind/unionfind.hpp
+++ datastructure/unionfind/unionfind.hpp
@@ -4,9 +4,10 @@
 
 struct UnionFind {
 private:
-  vec<int> _parents, _size;
+  int _n;
+  vec<int> _parents;
 public:
-  explicit UnionFind(int n): _parents(n), _size(n, 1) {
+  explicit UnionFind(int n): _n(n), _parents(n) {
     iota(all(_parents), 0);
   }
   int find(int a) {
@@ -18,17 +19,23 @@ public:
     b = find(b);
     if(a != b) {
       // merge後の親はa
-      if(_size[a] < _size[b]) swap(a, b);
-      _size[a] += _size[b];
       _parents[b] = a;
     }
   }
   bool same(int a, int b) {
     return find(a) == find(b);
   }
-  int size(int a) {
-    return _size[find(a)];
+  vec<vec<int>> groups() {
+    vec<int> leaders(_n), group_size(_n);
+    rep(i, _n) {
+      leaders[i] = find(i);
+      group_size[leaders[i]]++;
+    }
+    vec<vec<int>> res(_n);
+    rep(i, _n) res[i].reserve(group_size[i]);
+    rep(i, _n) res[leaders[i]].push_back(i);
+    res.erase(remove_if(all(res), [&](const vec<int>& v) { return v.empty(); }), res.end());
+    return res;
   }
-  // vec<vec<int>> groups() {}
 };
 using UF = UnionFind;
--- datastructure/unionfind/unionfind.md
+++ datastructure/unionfind/unionfind.md
@@ -5,9 +5,20 @@ documentation_of: ./unionfind.hpp
 
 ## Method
 
-- `UF UnionFind(int n)` : $O(n)$
-- `int find(int a)` : $O(\alpha(n))$
-- `void merge(int a)` : $O(\alpha(n))$
-- `bool same(int a)` : $O(\alpha(n))$
-- `int size(int a)` : $O(\alpha(n))$
+- `UF UnionFind(int n)`
+  $O(n)$
+  N頂点のUnionFindを生成する
+- `int find(int a)`
+  $O(\alpha(n))$
+  頂点`a`の根となる頂点を返す
+- `void merge(int a, int b)`
+  $O(\alpha(n))$
+  頂点`a`、頂点`b`を連結する
+- `bool same(int a, int b)`
+  $O(\alpha(n))$
+  頂点`a`、頂点`b`が連結かどうかを判定する
+- `vec<vec<int>> groups()`
+  $O(n)$ NoVerify
+  各連結成分の情報を出力する
+  各配列内は昇順(多分)
 
