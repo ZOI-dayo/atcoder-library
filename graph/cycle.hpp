522ec9d843e98f91c963d8a4dd0929203db9fecc
ZOI_dayo <zoizoi.dayo@gmail.com>
2023-08-28 11:47:52 +0900

    閉路検出を追加

---
+++ graph/cycle.hpp
@@ -0,0 +1,17 @@
+#include "template.hpp"
+
+bool has_cycle(Graph &g, int start = 0) {
+  vec<int> seen(g.size(), 0);
+  vec<int> finished(g.size(), 0);
+  auto dfs = [&](auto fn, int index) {
+    seen[index] = 1;
+    for(auto next : g[index]) {
+      if(finished[next]) continue;
+      if (seen[next] && !finished[next]) return true;
+      if (fn(fn, next)) return true;
+    }
+    finished[index] = 1;
+    return false;
+  };
+  return dfs(dfs, start);
+}
---
+++ test/aoj/GRL_4_A__cycle.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM                                                                \
+  "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/4/GRL_4_A"
+
+#include "../../graph/cycle.hpp"
+
+int main() {
+  int V, E;
+  cin >> V >> E;
+  Graph graph(V);
+  rep(i, E) {
+    int s, t;
+    cin >> s >> t;
+    graph[s].emplace_back(t);
+  }
+  bool result = false;
+  rep(i, V) {
+    result |= has_cycle(graph, i);
+  }
+  cout << result << endl;
+}
