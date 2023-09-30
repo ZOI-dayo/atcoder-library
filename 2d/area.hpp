24c11c204e295f2c877bb54a28300280335f7386
ZOI_dayo <zoizoi.dayo@gmail.com>
2023-10-01 01:39:48 +0900

    2次元平面を追加

---
+++ 2d/area.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include "../common/template.hpp"
+
+class Area {
+public:
+  int H, W;
+  bool allow_outside = false;
+  char out_val;
+  Area(int H, int W) : H(H), W(W), data(vector(H, string(W, '.'))) {}
+  Area(int H, int W, char out_val) : H(H), W(W), data(vector(H, string(W, '.'))), allow_outside(true), out_val(out_val) {}
+  string &operator[](int i) {
+    return data[i];
+  }
+  // 時計回りで90度回転
+  Area rotate90() const {
+    assert(H == W);
+    Area ret(W, H);
+    rep(i, H) rep(j, W) ret[j][H - i - 1] = data[i][j];
+    return ret;
+  }
+  Area rotated90() {
+    data = rotate90().data;
+  }
+  char at(int x, int y) {
+    if(is_contained(H, W, x, y)) return data[x][y];
+    else if(allow_outside) return out_val;
+    else {
+      cerr << "[Area::at] out of range" << endl;
+      assert(false);
+    }
+  }
+  void set(int x, int y, char val) {
+    if(is_contained(H, W, x, y)) data[x][y] = val;
+    else if(allow_outside) return;
+    else {
+      cerr << "[Area::set] out of range" << endl;
+      assert(false);
+    }
+  }
+private:
+  vec<string> data;
+};
+ostream &operator<<(ostream &os, Area &a) {
+  rep(i, a.H) os << a[i] << endl;
+  return os;
+}
+istream &operator>>(istream &is, Area &a) {
+  rep(i, a.H) is >> a[i];
+  return is;
+}
