0bdb5f7e33bac2814b1abf66a2c41333afd2996d
ZOI_dayo <zoizoi.dayo@gmail.com>
2023-11-01 20:29:24 +0900

    runtimepathを追加

--- build-pdf.sh
+++ build-pdf.sh
@@ -9,7 +9,7 @@ mkdir pdf
 for file in `ls **/*.hpp`; do
   mkdir -p pdf/`dirname ${file}`
 
-  nvim ${file} -c 'colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | w tmp.html | qa!'
+  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | w tmp.html | qa!'
   wkhtmltopdf --page-size B4 -O ${opt} --header-left "${commitid} ${file}" --header-right "[page]/[topage]" --header-font-name "Cica" --header-spacing 2 --no-background --margin-top 10 --margin-right 3 --margin-left 4 --margin-bottom 4 tmp.html pdf/${file}.pdf
   rm -f tmp.html
 
