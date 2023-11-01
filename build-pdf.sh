7a5a5626004d6c43a442538de5cd64de1c5cfa93
ZOI_dayo <zoizoi.dayo@gmail.com>
2023-11-01 20:09:01 +0900

    ミス修正

--- build-pdf.sh
+++ build-pdf.sh
@@ -9,7 +9,7 @@ mkdir pdf
 for file in `ls **/*.hpp`; do
   mkdir -p pdf/`dirname ${file}`
 
-  MYVIMRC="empty" nvim ${file} -c 'colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | w tmp.html | qa!'
+  nvim ${file} -c 'colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | w tmp.html | qa!'
   wkhtmltopdf --page-size B4 -O ${opt} --header-left "${commitid} ${file}" --header-right "[page]/[topage]" --header-font-name "Cica" --header-spacing 2 --no-background --margin-top 10 --margin-right 3 --margin-left 4 --margin-bottom 4 tmp.html pdf/${file}.pdf
   rm -f tmp.html
 
