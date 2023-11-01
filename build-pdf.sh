293b1286038d33a39fa84b441d2cc66cb3c2f759
ZOI_dayo <zoizoi.dayo@gmail.com>
2023-11-01 20:44:40 +0900

    TSBufDisableを削除

--- build-pdf.sh
+++ build-pdf.sh
@@ -19,12 +19,14 @@ for file in `ls **/*.hpp`; do
   echo 3
   nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | qa!'
   echo 4
-  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | qa!'
-  echo 5
-  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | qa!'
-  echo 6
-  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | w tmp.html | qa!'
-  echo 7
+  # nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | qa!'
+  # echo 5
+  # nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | qa!'
+  # echo 6
+  # nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | w tmp.html | qa!'
+  # echo 7
+  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TOhtml | w tmp.html | qa!'
+  echo 8
   wkhtmltopdf --page-size B4 -O ${opt} --header-left "${commitid} ${file}" --header-right "[page]/[topage]" --header-font-name "Cica" --header-spacing 2 --no-background --margin-top 10 --margin-right 3 --margin-left 4 --margin-bottom 4 tmp.html pdf/${file}.pdf
   rm -f tmp.html
 
