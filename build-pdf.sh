903d90fcf030dd7fd771512f896fa6d7a2133046
ZOI_dayo <zoizoi.dayo@gmail.com>
2023-11-01 20:36:29 +0900

    デバッグ

--- build-pdf.sh
+++ build-pdf.sh
@@ -9,6 +9,12 @@ mkdir pdf
 for file in `ls **/*.hpp`; do
   mkdir -p pdf/`dirname ${file}`
 
+  nvim ${file} -c 'qa!'
+  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | qa!'
+  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | qa!'
+  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | qa!'
+  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | qa!'
+  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | qa!'
   nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TSBufDisable highlight | TOhtml | w tmp.html | qa!'
   wkhtmltopdf --page-size B4 -O ${opt} --header-left "${commitid} ${file}" --header-right "[page]/[topage]" --header-font-name "Cica" --header-spacing 2 --no-background --margin-top 10 --margin-right 3 --margin-left 4 --margin-bottom 4 tmp.html pdf/${file}.pdf
   rm -f tmp.html
