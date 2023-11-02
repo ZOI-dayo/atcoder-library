6a1dbbb2ce108cb7cc71f8cd997a22bafcab2cf6
ZOI_dayo <zoizoi.dayo@gmail.com>
2023-11-02 17:52:31 +0900

    vimのフォント指定

--- build-pdf.sh
+++ build-pdf.sh
@@ -5,11 +5,11 @@ cd `dirname $0`
 opt=${2:-Portrait}
 mkdir pdf
 for file in `ls **/*.hpp`; do
-  commitid=`git log -n 1 --pretty=format:%H -- ${file}`
+  commitid=`git log -n 1 --pretty=format:%H ${file}`
   commitid=${commitid:0:7}
   mkdir -p pdf/`dirname ${file}`
 
-  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | TOhtml | w tmp.html | qa!'
+  nvim ${file} -c 'set runtimepath+=~/.config/nvim/nightfox.nvim | colorscheme dayfox | set number | let g:html_font = "Cica" | TOhtml | w tmp.html | qa!'
   wkhtmltopdf --page-size B4 -O ${opt} --header-left "${commitid} ${file}" --header-right "[page]/[topage]" --header-font-name "Cica" --header-spacing 2 --no-background --margin-top 10 --margin-right 3 --margin-left 4 --margin-bottom 4 tmp.html pdf/${file}.pdf
   rm -f tmp.html
 
