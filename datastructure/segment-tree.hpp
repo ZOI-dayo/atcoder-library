93f1c43d2340bc912a48ffa1f78c7b1d340c5730
ZOI_dayo <zoizoi.dayo@gmail.com>
2024-04-30 09:53:18 +0900

    Monoidを使わないように変更

--- datastructure/segment-tree.hpp
+++ datastructure/segment-tree.hpp
@@ -5,65 +5,92 @@
 
 // 参考: https://qiita.com/ningenMe/items/bf66de877e3b97d35862
 
-template <MonoidConcept M> struct SegmentTree {
-  using T = typename M::T;
+template <typename T> struct SegmentTree {
 
 private:
-  M _monoid;
+  /// 単位元
   T e;
+  /// 演算
+  function<T(T, T)> op;
 
-  // 要素数
-  int _n;
-  // SegTreeのノード上のデータ
-  vec<T> _data;
+  /// 要素数
+  int n;
+  /// SegTreeのノード上のデータ
+  vec<T> data;
+
+  /// 左の子のインデックスを返す
+  #define L(i) (i << 1)
+  /// 右の子のインデックスを返す
+  #define R(i) (i << 1 | 1)
+  /// 親のインデックスを返す
+  #define P(i) (i >> 1)
 
 public:
-  explicit SegmentTree(int n) : _n(1), _monoid(M()), e(_monoid.e()) {
-    while (_n < n)
-      _n *= 2;
-    _data = vec<T>(2 * _n, e);
-  }
-  explicit SegmentTree(const vec<T> &v) : _n(1), _monoid(M()), e(_monoid.e()) {
-    while (_n < v.size())
-      _n *= 2;
-    _data = vec<T>(2 * _n, e);
-    rep(i, v.size()) _data[i + _n] = v[i];
-    for (int i = _n - 1; i > 0; --i)
-      _data[i] = _monoid.op(_data[i << 1], _data[i << 1 | 1]);
+  /**
+    * @brief 個数から空のSegmentTreeを生成する
+    *
+    * @param n 要素数
+    * @param e 単位元
+    * @param op 演算
+    */
+  explicit SegmentTree(int n, T e, function<T(T,T)> op) : e(e), op(op) {
+    n = bit_ceil(n);
+    data = vec<T>(2 * n, e);
   }
 
-  void set(int i, T a) {
-    // 1段目は1, 2段目は2~3, ... , 最下段はn~(2n-1)
-    // より、iの最下段はi+n
-    i += _n;
-    _data[i] = a;
-    // i >>= 1で、iを親のindexに変更
-    // また、i==0となってしまうなら、i==1(頂点)であるので、終了
-    // i<<1は、(更新後の)iの左の子
-    // i<<1|1は、(更新後の)iの右の子 (+1を|1で表現)
-    while (i >>= 1)
-      _data[i] = _monoid.op(_data[i << 1], _data[i << 1 | 1]);
+  /**
+    * @brief 既存のベクトルからSegmentTreeを生成する
+    *
+    * @param v データ
+    * @param e 単位元
+    * @param op 演算
+    */
+  explicit SegmentTree(const vec<T> &v, T e, function<T(T,T)>) : e(e), op(op) {
+    n = bit_ceil(v.size());
+    data = vec<T>(2 * n, e);
+    rep(i, v.size()) data[i + n] = v[i];
+    for (int i = n - 1; i > 0; --i)
+      data[i] = op(data[L(i)], data[R(i)]);
   }
-  T get(int i) const { return _data[i + _n]; }
+
+  /**
+    * @brief i番目の要素を取得する
+    *
+    * @param i インデックス (0-indexed)
+    * @param a 更新後の要素
+    */
+  void set(int i, T a) {
+    i += n;
+    data[i] = a;
+    while (P(i))
+      data[i] = op(data[L(i)], data[R(i)]);
+  }
+
+  /**
+    * @brief i番目の要素を取得する
+    *
+    * @param i インデックス (0-indexed)
+    * @return i番目の要素
+    */
+  T get(int i) const { return data[i + n]; }
+
+  /**
+    * @brief [l, r)の範囲に対するクエリを行う
+    *
+    * @param l クエリの左端
+    * @param r クエリの右端
+    * @return クエリの結果
+    */
   T query(int l, int r) {
     T l_val = e, r_val = e;
-    // l, r(index)に+_nすると、最下段のseg-indexになる
-    // l < r == l,rをすべて含む区間にはたどり着いていない
-    // l/r >>= 1 : l/rを親に移動させる
-    for (l += _n, r += _n; l < r; l >>= 1, r >>= 1) {
-      // lが左の子ならば、l<=iの結果はlの親の結果であるので、計算しない
-      // lが右の子ならば、計算する必要がある
+    l+=n, r+=n;
+    while (l < r) {
+      l = P(l), r = P(r);
       if (l & 1)
-        l_val = _monoid.op(l_val, _data[l++]);
-      // l++をしても、l>>1が変わることはない
-      // ただし、r=l+1のとき、答えはここのlである
-      // ので、l<rに引っかかるようにして終了させている?
-
-      // rが左の子ならば、rの親はギリギリ使われない、すなわちrの条件を満たす
-      // rが右の子ならば、r-1は範囲に含まれるので、計算する必要がある
+        l_val = op(l_val, data[l++]);
       if (r & 1)
-        r_val = _monoid.op(_data[--r], r_val);
+        r_val = op(data[--r], r_val);
     }
-    return _monoid.op(l_val, r_val);
+    return op(l_val, r_val);
   }
 };
