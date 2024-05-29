809bd99786dad457aade99f66206aba379f43b66
ZOI_dayo <zoizoi.dayo@gmail.com>
2024-05-29 17:52:08 +0900

    make code readable

--- datastructure/dynamic-segment-tree.hpp
+++ datastructure/dynamic-segment-tree.hpp
@@ -18,50 +18,70 @@ private:
   /// 要素数
   int n;
 
+  /// ノードの構造体
   struct Node {
+
+    /// ノードの値
     T val;
+
+    /// 初期化用の値
+    T e;
+
+    /// 親ノード, 左の子ノード, 右の子ノード
     Node *p, *l_child, *r_child;
+
+    /// ノードの範囲
     int l, r;
-    Node(T val, int l, int r) :
-      val(val), p(nullptr), l_child(nullptr), r_child(nullptr),
+
+    Node(T val, T e, int l, int r) :
+      val(val), e(e),
+      p(nullptr), l_child(nullptr), r_child(nullptr),
       l(l), r(r) {}
-    Node(T val, Node *p, bool is_l_child) :
-      val(val), p(p), l_child(nullptr), r_child(nullptr),
-      l(is_l_child ? p->l : p->mid()),
-      r(is_l_child ? p->mid() : p->r) {}
-    int size() const { return r - l; }
+
+    Node(T val, Node *p, bool child_index) :
+      val(val), e(p->e),
+      p(p), l_child(nullptr), r_child(nullptr),
+      l(!child_index ? p->l : p->mid()),
+      r(!child_index ? p->mid() : p->r) {}
+
+    /// ノードの範囲の中央
     int mid() const { return (l + r) / 2; }
-    Node* child(bool is_l_child, T e) {
-      if(is_l_child) {
-        if(l_child == nullptr) l_child = new Node(e, this, true);
+
+    /// 子ノードを取得する
+    Node* child(bool child_index) {
+      if(child_index == 0) {
+        if(l_child == nullptr) l_child = new Node(e, this, 0);
         return l_child;
       } else {
-        if(r_child == nullptr) r_child = new Node(e, this, false);
+        if(r_child == nullptr) r_child = new Node(e, this, 1);
         return r_child;
       }
     }
-    bool is_l_area(int i) const { return i < mid(); }
+
+    /// ノードが葉かどうか判定する
     bool is_bottom() const { return l + 1 == r; }
-    bool is_l_child() const { return p->l_child == this; }
   };
 
   Node* root;
 
 public:
   /**
-   * @brief 個数から空のSegmentTreeを生成する O(n)
+   * @brief 個数から空のSegmentTreeを生成する O(1)
    *
    * @param length 要素数
    * @param e 単位元
    * @param op 演算
    */
   inline explicit DynamicSegmentTree(int length, T e, function<T(T, T)> op)
-      : e(e), op(op), n(bit_ceil(length)), root(new Node(e, 0, n)) {}
+      : e(e), op(op), n(bit_ceil(length)), root(new Node(e, e, 0, n)) {}
 
+  /**
+    * @brief i番目の要素を取得する O(log n)
+    */
   inline Node* get_node(int i) const {
     Node *node = root;
-    while(!node->is_bottom()) {
-      node = node->child(node->is_l_area(i), e);
+    while(node->l + 1 < node->r) {
+      node = node->child(node->mid() <= i);
     }
     return node;
   }
@@ -76,13 +96,13 @@ public:
     Node *node = get_node(i);
     node->val = a;
     while(node->p != nullptr) {
-      node->p->val = op(node->p->child(1, e)->val, node->p->child(0,e)->val);
       node = node->p;
+      node->val = op(node->child(0)->val, node->child(1)->val);
     }
   }
 
   /**
-   * @brief i番目の要素を取得する O(1)
+   * @brief i番目の要素を取得する O(log n)
    *
    * @param i インデックス (0-indexed)
    * @return i番目の要素
@@ -99,10 +119,19 @@ public:
   inline T query(int l, int r) const {
     return _query(root, l, r);
   }
+
+  /**
+    * @brief 特定のノードに対し、[l, r)の範囲に対するクエリを行う O(log n) * op
+    *
+    * @param node クエリを行うノード
+    * @param l クエリの左端 (0-indexed)
+    * @param r クエリの右端 (0-indexed)
+    * @return クエリの結果
+    */
   inline T _query(Node *node, int l, int r) const {
     if(r <= node->l || node->r <= l) return e;
     if(l <= node->l && node->r <= r) return node->val;
-    return op(_query(node->child(1, e), l, r), _query(node->child(0, e), l, r));
+    return op(_query(node->child(0), l, r), _query(node->child(1), l, r));
   }
 
 };
