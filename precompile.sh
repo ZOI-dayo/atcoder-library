07f2f2208d778bbeefed17500481ba3ee13f85af
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-07-07 08:34:42 +0900

    プリコンパイルスクリプトの追加

---
+++ precompile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+OPTIONS=`cat $(acc config-dir)/cpp/compile_flags.txt | tr '\n' ' '`
+
+for i in `find . -name "*.hpp"`; do
+  target=${i}.gch
+  if [ ! -e $target ] || [ -n "`find $i -newer $target -print`" ]; then
+    echo "precompiling $i ..."
+    g++ $OPTIONS -x c++-header -o $target $i
+  else
+    echo "precompile skipped $i"
+  fi
+done
+
